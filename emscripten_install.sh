e8922e7f5f3a6a4e80421fd8ba2cca7f378f595d
Sean Moore <sean@219design.com>
2019-05-23 05:48:42 -0400

    Allow targeting of install location.

--- emscripten_install.sh
+++ emscripten_install.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+TARGET="$1"
+mkdir -p $TARGET
+
 ./emscripten.sh
-cp web_src/* /var/www/html/stl/
-cp build_emscripten/* /var/www/html/stl/
+cp web_src/* $TARGET
+cp build_emscripten/* $TARGET
 
